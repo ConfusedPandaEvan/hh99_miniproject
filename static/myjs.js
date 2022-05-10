32f27063de8856b4543a6b2d3aa5a25a59b4c147
ConfusedPandaEvan <hwl278@nyu.edu>
2022-05-10 17:27:02 +0900

    Posting and Getting Posts

--- static/myjs.js
+++ static/myjs.js
@@ -1,5 +1,3 @@
-
-
 function post() {
     let name = $('#input-name').val()
     let file = $('#input-pic')[0].files[0]
@@ -26,10 +24,44 @@ function post() {
                 $("#modal-post").removeClass("is-active")
                 window.location.reload()
 
-            }
-            else
+            } else
                 alert("cool")
         }
     });
 }
 
+function get_posts(username) {
+    if (username == undefined) {
+        username = ""
+    }
+    $("#post-box").empty()
+    $.ajax({
+        type: "GET",
+        url: `/get_posts?username_give=${username}`,
+        data: {},
+        success: function (response) {
+            if (response["result"] == "success") {
+
+                let posts = response["posts"]
+                console.log(posts)
+                for (let i = 0; i < posts.length; i++) {
+                    let post = posts[i]
+                    let time_post = new Date(post["date"])
+                    // let time_before = time2str(time_post)
+
+
+                    let html_temp = `<div class="today">
+                                        <img class="today_image2" src="/static/${post['post_pic_real']}" alt="Image">
+                                        </img>
+                                        <div class="today_image1">
+                                        </div>
+                                        <a>${post['username']}님의 빠숑</a>
+                                        <p>기온 : <span id="temp"> 21.3</span>℃</p>
+                                        <p>코디설명: ${post['post_info']}</p>
+                                    </div>`
+                    $("#post-box").append(html_temp)
+                }
+            } else console.log(wtf)
+        }
+    })
+}
